ab097803ebcef34f9962c69e4c4d2d6367faab7e
Mary <marieshix98@gmail.com>
2023-03-26 23:56:01 +0300

    Register routes

---
+++ src/routes/AllRoutes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+const Home = React.lazy(()=> import('../pages/Home'))
+const Error = React.lazy(() =>import('../pages/Error'))
+const routesList = [
+    {
+        path: "/",
+        component:<Home/>
+    },
+    {
+        path: "*",
+        component:<Error/>
+    }
+]
+const AllRoutes = () =>{
+    return <Routes>{routesList.map(path =><Route path={path.path} element={path.component}/>)}</Routes>
+}
