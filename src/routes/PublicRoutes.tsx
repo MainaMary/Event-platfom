d30fe94f502e5e89f92dc5cf359aa6ca28e647cc
Mary <marieshix98@gmail.com>
2023-03-27 23:41:16 +0300

    Configure public and private routes

---
+++ src/routes/PublicRoutes.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+const PublicRoutes = () => {
+  return (
+    <div>PublicRoutes</div>
+  )
+}
+
+export default PublicRoutes
