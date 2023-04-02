cd501ad4490a32b3f2730f8118f8d7d8c7712882
Mary <marieshix98@gmail.com>
2023-04-02 23:58:18 +0300

    Define search param

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { baseUrl } from '../url/baseUrl'
+import CustomInput from '../components/CustomInput'
 
 const Categories = () => {
+  const [search, setSearch] = useState<string>('')
+  const handleChange =(e:any)=>{
+    e.preventDefault()
+    setSearch(e.target.value)
+  }
     const fetchEventCategory = async () =>{
         try{
         const response = await axios.get(`https://www.eventbriteapi.com/v3/subcategories/&API_TOKEN=${import.meta.env.REACT_APP_API_TOKEN}`)
@@ -26,8 +32,11 @@ const Categories = () => {
       if (isError) {
         return <span>Error: {error.message}</span>
       }
+    
   return (
-    <div>Details</div>
+    <div>
+      <CustomInput value={search} onChange={handleChange}/>
+    </div>
   )
 }
 
