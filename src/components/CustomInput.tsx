7de1c0d1cae8b3431f5282f8f724679b182f2ddf
Mary <marieshix98@gmail.com>
2023-04-02 23:57:39 +0300

    Define custom input component

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Inputprops } from '../model/types'
 
-const CustomInput = () => {
+const CustomInput = (props:Inputprops) => {
+  const {type, name, onChange, min, max, placeholder, accept, value, required, multiple}= props
   return (
-    <div>CustomInput</div>
+    <input accept={accept} type={type} min={min} max={max} name={name} value={value} onChange={onChange} required={required} multiple={multiple} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder}/>
   )
 }
 
